036f99b10000682512a222af90ea480deb435a25
Novel Tieng <tien@sitecore.net>
2023-12-21 23:44:47 +0800

    first-commit

---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { headerLinks } from "@/contants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavItems = () => {
+  const pathname = usePathname();
+  return (
+    <ul className="md:flex-between md:flex-row w-full flex flex-col items-start gap-5">
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={link.route}
+            className={`${
+              isActive && "text-primary-500"
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default NavItems;
